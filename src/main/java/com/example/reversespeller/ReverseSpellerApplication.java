df010e6569a4e177798a226aaeeae3c0aef2f3f5
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-08 03:02:28 +0200

    initial commit

---
+++ src/main/java/com/example/reversespeller/ReverseSpellerApplication.java
@@ -0,0 +1,12 @@
+package com.example.reversespeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseSpellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseSpellerApplication.class, args);
+	}
+}
