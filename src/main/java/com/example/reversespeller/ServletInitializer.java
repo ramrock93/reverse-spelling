e967a03ab9b0cc5d0b01c1f8f9e226bae482976e
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-08 21:30:02 +0200

    Adds comment to code in ServletInitializer class.

--- src/main/java/com/example/reversespeller/ServletInitializer.java
+++ src/main/java/com/example/reversespeller/ServletInitializer.java
@@ -3,6 +3,23 @@ package com.example.reversespeller;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * An opinionated WebApplicationInitializer to run a SpringApplication from a
+ * traditional WAR deployment. Binds Servlet, Filter and
+ * ServletContextInitializer beans from the application context to the servlet
+ * container.
+ * 
+ * To configure the application either override the
+ * configure(SpringApplicationBuilder) method (calling
+ * SpringApplicationBuilder.sources(Object)) or make the initializer itself
+ * a @Configuration. If you are using SpringBootServletInitializer in
+ * combination with other WebApplicationInitializers you might also want to add
+ * an @Ordered annotation to configure a specific startup order.
+ * 
+ * @author Ramin Esfandiari </br>
+ *         8. aug. 2017 </br>
+ *
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
