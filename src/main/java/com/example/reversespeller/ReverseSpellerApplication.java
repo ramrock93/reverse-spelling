a31da4496b1229893de32b72511ee0d1375de96f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-08 21:28:13 +0200

    Adds comment to code in ReverseSpellerApplication class.

--- src/main/java/com/example/reversespeller/ReverseSpellerApplication.java
+++ src/main/java/com/example/reversespeller/ReverseSpellerApplication.java
@@ -3,6 +3,14 @@ package com.example.reversespeller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The ReverseSpellerApplication is the main class in reverse spell web
+ * application. Its responsible to run the application.
+ * 
+ * @author Ramin Esfandiari </br>
+ *         8. aug. 2017 </br>
+ *
+ */
 @SpringBootApplication
 public class ReverseSpellerApplication {
 
