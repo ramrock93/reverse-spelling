851ebe452e66b5e11b242186ddf4568ea98b0523
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-08 14:23:01 +0200

    Adds styling and alignments to the UI components.

--- src/main/java/com/example/reversespeller/ReverseSpellerUI.java
+++ src/main/java/com/example/reversespeller/ReverseSpellerUI.java
@@ -3,46 +3,64 @@ package com.example.reversespeller;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.swing.LayoutFocusTraversalPolicy;
-
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.ui.Alignment;
 
 @SpringUI
 public class ReverseSpellerUI extends UI {
 
 	private VerticalLayout vLayout;
-	private HorizontalLayout hLayout;
+	private GridLayout grid;
 
+	private Label header;
 	private TextField input;
 	private Button submit;
 	private Label result;
+	private Label resultHeader;
 
 	@Override
 	protected void init(VaadinRequest request) {
+		grid = new GridLayout(3, 3);
+		grid.setWidth("100%");
+		grid.setHeight("100%");
+
 		vLayout = new VerticalLayout();
-		hLayout = new HorizontalLayout();
+		vLayout.setWidth("100%");
 
-		input = new TextField("Text to reverse:");
-		result = new Label("Result:");
+		header = new Label("Reverse Speller");
+		header.addStyleName(ValoTheme.LABEL_H1);
+		header.addStyleName(ValoTheme.LABEL_BOLD);
+
+		input = new TextField("Your name:");
+		input.addStyleName(ValoTheme.TEXTFIELD_HUGE);
+
+		resultHeader = new Label("Your name spelled backwards:");
+
+		result = new Label();
+		result.addStyleName(ValoTheme.LABEL_H1);
+
 		submit = new Button("Reverse");
+		submit.addStyleName(ValoTheme.BUTTON_PRIMARY);
+		submit.addStyleName(ValoTheme.BUTTON_HUGE);
 
-		hLayout.addComponents(input, submit);
-		hLayout.addStyleName(ValoTheme.LAYOUT_HORIZONTAL_WRAPPING);
-		hLayout.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
-
-		vLayout.addComponents(hLayout, result);
+		vLayout.addComponents(input, submit, resultHeader, result);
+		vLayout.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+		vLayout.setComponentAlignment(input, Alignment.MIDDLE_CENTER);
+		vLayout.setComponentAlignment(submit, Alignment.MIDDLE_CENTER);
+		vLayout.setComponentAlignment(result, Alignment.MIDDLE_CENTER);
+		vLayout.setComponentAlignment(resultHeader, Alignment.MIDDLE_CENTER);
 
 		submit.addClickListener(e -> {
 			String text = input.getValue().replaceAll(" ", "%20");
@@ -59,7 +77,10 @@ public class ReverseSpellerUI extends UI {
 			result.setValue(reversed);
 		});
 
-		setContent(vLayout);
+		grid.addComponent(header, 1, 0);
+		grid.setComponentAlignment(header, Alignment.TOP_CENTER);
+		grid.addComponent(vLayout, 1, 1);
+		setContent(grid);
 	}
 
 }
