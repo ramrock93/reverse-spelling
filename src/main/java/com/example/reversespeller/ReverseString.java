8db40b4fc07ed3b0b5adbb3079e386e7ef50bf83
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-08 21:24:55 +0200

    Adds comment to code in ReverseString class.

--- src/main/java/com/example/reversespeller/ReverseString.java
+++ src/main/java/com/example/reversespeller/ReverseString.java
@@ -1,11 +1,26 @@
 package com.example.reversespeller;
 
+/**
+ * The ReverseString class contains one class method that takes in a string
+ * parameter and returns the revered string.
+ * 
+ * @author Ramin Esfandiari </br>
+ *         8. aug. 2017 </br>
+ *
+ */
 public class ReverseString {
 
+	/**
+	 * Takes in a string parameter, and returns the reversed string.
+	 * 
+	 * @param string
+	 *            The string to reverse.
+	 * @return The reversed string.
+	 */
 	public static String reverseString(String string) {
-		StringBuilder sb = new StringBuilder();
-		sb.append(string);
+		StringBuilder sb = new StringBuilder(); // Instantiate a new StringBuilder object.
+		sb.append(string); // Append the string to reverse to the StringBuilder.
 
-		return sb.reverse().toString();
+		return sb.reverse().toString(); // Reverse the string and return it.
 	}
 }
